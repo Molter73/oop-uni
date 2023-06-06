8f7670d9f71c66c80c90a8eed6c1eae7abbf6260
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-06-06 11:34:26 +0200

    Add docs for ATM

--- atm/src/Main.java
+++ atm/src/Main.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Thrown when a transaction causes the balance in an account to become
+ * negative.
+ */
 class InsufficientBalanceException extends Exception {
 	public InsufficientBalanceException(String balance, String amount) {
 		super("No se puede retirar €" + amount + "\nSaldo disponible: €" + balance);
@@ -9,15 +13,28 @@ class InsufficientBalanceException extends Exception {
 	}
 }
 
+/**
+ * Thrown when the user attempts to create an account with a negative balance.
+ */
 class NegativeBalanceException extends Exception {
 	public NegativeBalanceException(String balance) {
 		super("No se puede crear cuentas con saldo negativo: €" + balance);
 	}
 }
 
+/**
+ * Handling of account information.
+ */
 class Account {
 	private Integer balance;
 
+	/**
+	 * Create a new {@link Account} with the provided balance.
+	 *
+	 * @param balance The balance for the new account being created.
+	 * @throws NegativeBalanceException Thrown when the provided balance is less
+	 *                                  than 0.
+	 */
 	public Account(Integer balance) throws NegativeBalanceException {
 		if (balance < 0) {
 			throw new NegativeBalanceException(amountToString(balance));
@@ -26,14 +43,31 @@ class Account {
 		this.balance = balance;
 	}
 
+	/**
+	 * Getter for the balance.
+	 *
+	 * @return The balance in the account.
+	 */
 	public Integer getBalance() {
 		return balance;
 	}
 
+	/**
+	 * Perform a deposit transaction on the account.
+	 *
+	 * @param amount The amount to be added to the account.
+	 */
 	public void deposit(Integer amount) {
 		balance += amount;
 	}
 
+	/**
+	 * Perform a withdrawal transaction on the account.
+	 *
+	 * @param amount The amount to be withdrawn.
+	 * @throws InsufficientBalanceException Thrown when there is not enough cash to
+	 *                                      perform the withdrawal.
+	 */
 	public void withdraw(Integer amount) throws InsufficientBalanceException {
 		if (balance < amount) {
 			throw new InsufficientBalanceException(toString(), amountToString(amount));
@@ -42,6 +76,12 @@ class Account {
 		balance -= amount;
 	}
 
+	/**
+	 * Translate an amount to a human readable format.
+	 *
+	 * @param amount The amount to translate.
+	 * @return A string with a human readable representation of the amount.
+	 */
 	static public String amountToString(Integer amount) {
 		Integer intPart = amount / 100;
 		Integer decPart = amount % 100;
@@ -63,6 +103,9 @@ enum MenuOption {
 	EXIT,
 }
 
+/**
+ * Interface between the user and their {@link Account}.
+ */
 class ATM {
 	private Account account;
 	private BufferedReader br;
@@ -71,6 +114,11 @@ class ATM {
 		br = new BufferedReader(new InputStreamReader(System.in));
 	}
 
+	/**
+	 * Print the main menu to the user and get their selection.
+	 *
+	 * @return A {@link MenuOption} depending on the selected option.
+	 */
 	private MenuOption menu() {
 		while (true) {
 			System.out.println("Seleccione la opción deseada:");
@@ -108,6 +156,12 @@ class ATM {
 		}
 	}
 
+	/**
+	 * Create a new {@link Account} with the provided balance.
+	 *
+	 * @param balance The balance the new {@link Account} should have.
+	 * @return A string describing the result of the operation.
+	 */
 	private String createAccount(Integer balance) {
 		try {
 			account = new Account(balance);
@@ -118,12 +172,26 @@ class ATM {
 		return "Creada cuenta con balance €" + account.toString();
 	}
 
+	/**
+	 * Perform a deposit transaction on the current {@link Account}.
+	 *
+	 * @param amount The amount to deposit into the account.
+	 * @return A string describing the result of the operation.
+	 * @throws NullPointerException Thrown when no account is selected.
+	 */
 	private String deposit(Integer amount) throws NullPointerException {
 		account.deposit(amount);
 
 		return "Depósito correcto. Nuevo saldo: €" + account.toString();
 	}
 
+	/**
+	 * Perform a withdrawal transaction on the current {@link Account}.
+	 *
+	 * @param amount The amount to withdraw from the {@link Account}.
+	 * @return A string describing the result of the operation.
+	 * @throws NullPointerException Thrown when no account is selected.
+	 */
 	private String withdraw(Integer amount) throws NullPointerException {
 		try {
 			account.withdraw(amount);
@@ -134,20 +202,47 @@ class ATM {
 		return "Extracción correcta. Nuevo saldo: €" + account.toString();
 	}
 
+	/**
+	 * Return a string with the balance in the {@link Account} in a human readable
+	 * format.
+	 *
+	 * @return a string with the balance in the {@link Account} in a human readable
+	 *         format.
+	 * @throws NullPointerException Thrown when no account is selected.
+	 */
 	private String checkBalance() throws NullPointerException {
 		return "Saldo disponible: €" + account.toString();
 	}
 
+	/**
+	 * Helper class for uniformly logging the result of a transaction.
+	 *
+	 * @param msg The message to log.
+	 */
 	private void logResult(String msg) {
 		System.out.println("El resultado de la operación es: " + msg);
 	}
 
+	/**
+	 * Ask the user to input an amount and turn it into an {@link Account}
+	 * compatible format.
+	 *
+	 * @return An integer with the input amount.
+	 * @throws IOException           Thrown when a system error occurs while reading
+	 *                               stdin.
+	 * @throws NumberFormatException Thrown when the number input by the user is in
+	 *                               a wrong format.
+	 */
 	private Integer getUserAmount() throws IOException, NumberFormatException {
 		System.out.print("Ingrese el monto deseado: €");
 		Float rawAmount = Float.parseFloat(br.readLine()) * 100;
 		return rawAmount.intValue();
 	}
 
+	/**
+	 * Run in a continuous loop, asking the user for input, performing transactions
+	 * and logging the result of said transactions.
+	 */
 	public void run() {
 		for (MenuOption option = menu(); option != MenuOption.EXIT; option = menu()) {
 			String result;
@@ -217,6 +312,7 @@ class ATM {
 					// we will never hit this point.
 					throw new RuntimeException("Opción inválida");
 			}
+
 			logResult(result);
 		}
 	}
